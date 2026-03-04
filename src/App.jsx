758a32715958d363a0021b2243da909911f60570
zahraai3 <zahraaibrahem241@gmail.com>
2026-03-04 17:34:48 +0300

    feat: add routing and page components for Home, Setup, and Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
+import Home from './pages/Home/Home'
+import Dashboard from './pages/Dashboard/Dashboard'
+import Setup from './pages/Setup/Setup'
+import { UserWaterProvider , DailyWaterProvider } from './context/Watercontext'
+import { createBrowserRouter,createRoutesFromElements ,Route , RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Home/>} >
+      <Route path='setting' element={<Setup/>} />
+      <Route path='dashboard' element={ <Dashboard/>}/>
+    </Route>
+  )
+)
 
 function App() {
   return (
     <div>
-      
+      <UserWaterProvider>
+        <DailyWaterProvider>
+          <RouterProvider router={router} />
+        </DailyWaterProvider>
+      </UserWaterProvider>
     </div>
   )
 }
