56b74ec3a0917b5081466c6a1d2c390e5450f9b1
zahraai3 <zahraaibrahem241@gmail.com>
2026-03-04 15:54:06 +0300

    feat: add UserWater and DailyWater context providers with hooks

--- src/context/Watercontext.jsx
+++ src/context/Watercontext.jsx
@@ -1,1 +1,43 @@
+import { createContext, useContext, useState } from "react";
 
+export const UserWaterContext = createContext(null);
+export const DailyWaterContext = createContext(null);
+
+
+export function UserWaterProvider({children}){
+    const [userSetting , setUserSetting] = useState({
+        dailyGoal:2000,
+        reminderInterval:60,
+        startTime:'09:00',
+        endTime: '11:00'
+    });
+
+    return(
+        <UserWaterContext.Provider value={{userSetting ,setUserSetting}}>
+            {children}
+        </UserWaterContext.Provider>
+    )
+}
+
+export function DailyWaterProvider({children}){
+    const [dailySetting , setDailySetting] =  useState({
+        date:new Date().toLocaleDateString(),
+        consumedAmount:0,
+        completed:false ,
+        history:[]
+    })
+
+    return(
+        <DailyWaterContext.Provider value={{dailySetting,setDailySetting}}>
+            {children}
+        </DailyWaterContext.Provider>
+    )
+}
+
+export function useUserWater(){
+    return useContext(UserWaterContext)
+}
+
+export function useDailyWater(){
+    return useContext(DailyWaterContext)
+}
