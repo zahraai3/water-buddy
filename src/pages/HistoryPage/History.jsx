1da81c198bf338e295773d430ffa171d0599adc1
zahraai3 <zahraaibrahem241@gmail.com>
2026-03-05 21:39:59 +0300

    add: logic of the history page

--- src/pages/HistoryPage/History.jsx
+++ src/pages/HistoryPage/History.jsx
@@ -3,11 +3,21 @@ import { getWaterHistory } from '../../utils/storage'
 
 function History() {
 
-    
+    const historyElements = () => {
+        const history = getWaterHistory() || []
+        return history.length === 0 ? <p>No History Yet</p> : history.map((item,index) => {
+            return(
+                <div key={index}>
+                    <h2>Day : {item.date}</h2>
+                    <h3>Consumed Amount : {item.consumedAmount}</h3>
+                    <h3>Completed : {item.completed ? "True" : "False"}</h3>
+                </div>
+            )
+        })
+    }
     return (
         <div>
-        <h1>Histoorryyyyyy</h1>
-        <button onClick={() => console.log(getWaterHistory())}> History </button>
+        {historyElements()}
         </div>
     )
 }
