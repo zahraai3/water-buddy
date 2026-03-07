929a570f7bc8a609f5c6f4f685eac277a1b1c49b
zahraai3 <zahraaibrahem241@gmail.com>
2026-03-07 17:37:25 +0300

    add: style for the setting page

--- src/pages/Setup/Setup.jsx
+++ src/pages/Setup/Setup.jsx
@@ -30,18 +30,25 @@ function Setup() {
         }
     )
 
-    
 
     return (
-    <div className='setup-container'>
-        <form action={formFunc}>
-            <input type="number" name='dailyGoal' defaultValue={userSetting.dailyGoal} step={100} min={1000} max={4000}/>
-            <input type="time" name='startTime' defaultValue={userSetting.startTime}/>
-            <input type="time" name='endTime' defaultValue={userSetting.endTime}/>
-            <input type="number" name='reminderInterval' defaultValue={userSetting.reminderInterval} step={5} min={15} max={120}/>
-            <button type='submit' disabled={isPending}>Submit</button>
-        </form>
-    </div>
+        <div className='setup-container'>
+            <form className="form" action={formFunc}>
+                <div className="title">
+                    <h1>Heellooooo</h1>
+                    <span>Take a sip of water and Customize Your Hydration.</span>
+                </div>
+                <label htmlFor="dailyGoal">Daily Goal (ml):</label>
+                <input  className='input'  type="number" name='dailyGoal' defaultValue={userSetting.dailyGoal} step={100} min={1000} max={4000}/>
+                <label htmlFor="startTime">Start Time:</label>
+                <input  className='input' type="time" name='startTime' defaultValue={userSetting.startTime}/>
+                <label htmlFor="endTime">End Time:</label>
+                <input  className='input' type="time" name='endTime' defaultValue={userSetting.endTime}/>
+                <label htmlFor="reminderInterval">Reminder Interval (minutes):</label>
+                <input  className='input' type="number" name='reminderInterval' defaultValue={userSetting.reminderInterval} step={5} min={15} max={120}/>
+                <button className="button-confirm">Let`s Start →</button>
+            </form>
+        </div>
     )
 }
 
