b310709539d5be265c3596b04cd986ae83b1ceae
zahraai3 <zahraaibrahem241@gmail.com>
2026-03-07 18:02:01 +0300

    add: implement responsive navbar with toggle functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
 import './Header.css'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { useState } from 'react'
 
 function Header() {
+
+    const [open, setOpen] = useState(false)
+
+    function toggleMenu(){
+        setOpen(!open)
+    }
+
     return (
         <div className='navbar'>
+            <button 
+                className={`hamburger ${open ? "open" : ""}`} 
+                aria-label="Toggle menu"
+                onClick={toggleMenu}
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
             <div className="nav-name">
                 <h1>Water Buddy</h1>
             </div>
