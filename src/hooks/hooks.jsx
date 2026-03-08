7ffb87ce7a0b31326667e7d0a41918173878576e
zahraai3 <zahraaibrahem241@gmail.com>
2026-03-08 22:38:55 +0300

    add: implement notification scheduling to remind users to drink water based on user settings

--- src/hooks/hooks.jsx
+++ src/hooks/hooks.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react"
 import { useNotificationContext } from "../context/Watercontext"
+import { useUserWater } from "../context/Watercontext"
 
 export function useRequestNotificationPermission() {
     const {permission , setPermission} = useNotificationContext()
@@ -15,5 +16,33 @@ export function useRequestNotificationPermission() {
             })
     }
     return requestPermissionNotification
+}
+
+export function useNotificationScheduler(){
+    const {permission} = useNotificationContext()
+    const {userSetting} = useUserWater()
+
+    useEffect(() =>{
+        if(!permission) return
+
+        const interval = setInterval(() => {
+            const now = new Date()
+            const currentTime = now.getHours() * 60 + now.getMinutes()
+
+            const [startH , startM] = userSetting.startTime.split(":").map(Number)
+            const [endH , endM] = userSetting.endTime.split(":").map(Number)
+            const startMinutes = startH * 60 + startM
+            const endMinutes = endH * 60 + endM
+
+            if(currentTime >= startMinutes && currentTime <= endMinutes){
+                new Notification("Time to drink water!", {
+                    body: "Stay hydrated and take a sip of water now!"
+                })
+            }
+        },userSetting.reminderInterval * 60 * 1000)
+    
+        return () => clearInterval(interval)
+
+    },[permission , userSetting])
 }
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import Header from '../../components/Header/Header'
-
+import { useNotificationScheduler } from "../../hooks/hooks"
 function Home() {
+
+    useNotificationScheduler()
+
     return (
         <div>
             <Header/>
